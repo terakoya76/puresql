bb61a346b7ea4c2ac460f29813d108294d1c2c25
hajime <d.runana@gmail.com>
2018-04-19 23:11:53 +0900

    adapt right_hand of condition to Target

--- src/executors/selector.rs
+++ src/executors/selector.rs
@@ -20,16 +20,16 @@ impl Clone for Box<Selector> {
 pub struct Equal {
     pub left_table: Option<String>,
     pub left_column: String,
-    pub right_column_offset: Option<usize>,
+    pub right_hand: Option<Target>,
     pub scholar: Option<Field>,
 }
 
 impl Equal {
-    pub fn new(left_hand: Target, right_column_offset: Option<usize>, scholar: Option<Field>) -> Box<Equal> {
+    pub fn new(left_hand: Target, right_hand: Option<Target>, scholar: Option<Field>) -> Box<Equal> {
         Box::new(Equal {
             left_table: left_hand.table_name,
             left_column: left_hand.name,
-            right_column_offset: right_column_offset,
+            right_hand: right_hand,
             scholar: scholar,
         })
     }
@@ -37,10 +37,10 @@ impl Equal {
 
 impl Selector for Equal {
     fn evaluate(&self, tuple: &Tuple, columns: &[Column]) -> Result<bool, SelectorError> {
-        match self.right_column_offset {
+        match self.right_hand {
             None => {},
-            Some(offset) => {
-                let ref right_side: Field = tuple.fields[offset];
+            Some(ref right_hand) => {
+                let ref right_side: Field = try!(find_field(tuple, columns, right_hand.table_name.clone(), right_hand.name.clone()));
                 let ref left_side: Field = try!(find_field(tuple, columns, self.left_table, self.left_column));
                 return Ok(left_side == right_side);
             },
@@ -78,16 +78,16 @@ impl Selector for Equal {
 pub struct NotEqual {
     pub left_table: Option<String>,
     pub left_column: String,
-    pub right_column_offset: Option<usize>,
+    pub right_hand: Option<Target>,
     pub scholar: Option<Field>,
 }
 
 impl NotEqual {
-    pub fn new(left_hand: Target, right_column_offset: Option<usize>, scholar: Option<Field>) -> Box<NotEqual> {
+    pub fn new(left_hand: Target, right_hand: Option<Target>, scholar: Option<Field>) -> Box<NotEqual> {
         Box::new(NotEqual {
             left_table: left_hand.table_name,
             left_column: left_hand.name,
-            right_column_offset: right_column_offset,
+            right_hand: right_hand,
             scholar: scholar,
         })
     }
@@ -95,10 +95,10 @@ impl NotEqual {
 
 impl Selector for NotEqual {
     fn evaluate(&self, tuple: &Tuple, columns: &[Column]) -> Result<bool, SelectorError> {
-        match self.right_column_offset {
+        match self.right_hand {
             None => {},
-            Some(offset) => {
-                let ref right_side: Field = tuple.fields[offset];
+            Some(ref right_hand) => {
+                let ref right_side: Field = try!(find_field(tuple, columns, right_hand.table_name.clone(), right_hand.name.clone()));
                 let ref left_side: Field = try!(find_field(tuple, columns, self.left_table, self.left_column));
                 return Ok(left_side != right_side);
             },
@@ -136,16 +136,16 @@ impl Selector for NotEqual {
 pub struct LT {
     pub left_table: Option<String>,
     pub left_column: String,
-    pub right_column_offset: Option<usize>,
+    pub right_hand: Option<Target>,
     pub scholar: Option<Field>,
 }
 
 impl LT {
-    pub fn new(left_hand: Target, right_column_offset: Option<usize>, scholar: Option<Field>) -> Box<LT> {
+    pub fn new(left_hand: Target, right_hand: Option<Target>, scholar: Option<Field>) -> Box<LT> {
         Box::new(LT {
             left_table: left_hand.table_name,
             left_column: left_hand.name,
-            right_column_offset: right_column_offset,
+            right_hand: right_hand,
             scholar: scholar,
         })
     }
@@ -153,10 +153,10 @@ impl LT {
 
 impl Selector for LT {
     fn evaluate(&self, tuple: &Tuple, columns: &[Column]) -> Result<bool, SelectorError> {
-        match self.right_column_offset {
+        match self.right_hand {
             None => {},
-            Some(offset) => {
-                let ref right_side: Field = tuple.fields[offset];
+            Some(ref right_hand) => {
+                let ref right_side: Field = try!(find_field(tuple, columns, right_hand.table_name.clone(), right_hand.name.clone()));
                 let ref left_side: Field = try!(find_field(tuple, columns, self.left_table, self.left_column));
                 return Ok(left_side < right_side);
             },
@@ -194,16 +194,16 @@ impl Selector for LT {
 pub struct LE {
     pub left_table: Option<String>,
     pub left_column: String,
-    pub right_column_offset: Option<usize>,
+    pub right_hand: Option<Target>,
     pub scholar: Option<Field>,
 }
 
 impl LE {
-    pub fn new(left_hand: Target, right_column_offset: Option<usize>, scholar: Option<Field>) -> Box<LE> {
+    pub fn new(left_hand: Target, right_hand: Option<Target>, scholar: Option<Field>) -> Box<LE> {
         Box::new(LE {
             left_table: left_hand.table_name,
             left_column: left_hand.name,
-            right_column_offset: right_column_offset,
+            right_hand: right_hand,
             scholar: scholar,
         })
     }
@@ -211,10 +211,10 @@ impl LE {
 
 impl Selector for LE {
     fn evaluate(&self, tuple: &Tuple, columns: &[Column]) -> Result<bool, SelectorError> {
-        match self.right_column_offset {
+        match self.right_hand {
             None => {},
-            Some(offset) => {
-                let ref right_side: Field = tuple.fields[offset];
+            Some(ref right_hand) => {
+                let ref right_side: Field = try!(find_field(tuple, columns, right_hand.table_name.clone(), right_hand.name.clone()));
                 let ref left_side: Field = try!(find_field(tuple, columns, self.left_table, self.left_column));
                 return Ok(left_side <= right_side);
             },
@@ -252,16 +252,16 @@ impl Selector for LE {
 pub struct GT {
     pub left_table: Option<String>,
     pub left_column: String,
-    pub right_column_offset: Option<usize>,
+    pub right_hand: Option<Target>,
     pub scholar: Option<Field>,
 }
 
 impl GT {
-    pub fn new(left_hand: Target, right_column_offset: Option<usize>, scholar: Option<Field>) -> Box<GT> {
+    pub fn new(left_hand: Target, right_hand: Option<Target>, scholar: Option<Field>) -> Box<GT> {
         Box::new(GT {
             left_table: left_hand.table_name,
             left_column: left_hand.name,
-            right_column_offset: right_column_offset,
+            right_hand: right_hand,
             scholar: scholar,
         })
     }
@@ -269,10 +269,10 @@ impl GT {
 
 impl Selector for GT {
     fn evaluate(&self, tuple: &Tuple, columns: &[Column]) -> Result<bool, SelectorError> {
-        match self.right_column_offset {
+        match self.right_hand {
             None => {},
-            Some(offset) => {
-                let ref right_side: Field = tuple.fields[offset];
+            Some(ref right_hand) => {
+                let ref right_side: Field = try!(find_field(tuple, columns, right_hand.table_name.clone(), right_hand.name.clone()));
                 let ref left_side: Field = try!(find_field(tuple, columns, self.left_table, self.left_column));
                 return Ok(left_side > right_side);
             },
@@ -310,16 +310,16 @@ impl Selector for GT {
 pub struct GE {
     pub left_table: Option<String>,
     pub left_column: String,
-    pub right_column_offset: Option<usize>,
+    pub right_hand: Option<Target>,
     pub scholar: Option<Field>,
 }
 
 impl GE {
-    pub fn new(left_hand: Target, right_column_offset: Option<usize>, scholar: Option<Field>) -> Box<GE> {
+    pub fn new(left_hand: Target, right_hand: Option<Target>, scholar: Option<Field>) -> Box<GE> {
         Box::new(GE {
             left_table: left_hand.table_name,
             left_column: left_hand.name,
-            right_column_offset: right_column_offset,
+            right_hand: right_hand,
             scholar: scholar,
         })
     }
@@ -327,10 +327,10 @@ impl GE {
 
 impl Selector for GE {
     fn evaluate(&self, tuple: &Tuple, columns: &[Column]) -> Result<bool, SelectorError> {
-        match self.right_column_offset {
+        match self.right_hand {
             None => {},
-            Some(offset) => {
-                let ref right_side: Field = tuple.fields[offset];
+            Some(ref right_hand) => {
+                let ref right_side: Field = try!(find_field(tuple, columns, right_hand.table_name.clone(), right_hand.name.clone()));
                 let ref left_side: Field = try!(find_field(tuple, columns, self.left_table, self.left_column));
                 return Ok(left_side >= right_side);
             },
@@ -364,16 +364,16 @@ impl Selector for GE {
     }
 }
 
-fn find_field(tuple: &Tuple, columns: &[Column], left_table: Option<String>, left_column: String) -> Result<Field, SelectorError> {
+fn find_field(tuple: &Tuple, columns: &[Column], table_name: Option<String>, column_name: String) -> Result<Field, SelectorError> {
     for column in columns {
-        if left_column != column.name {
+        if column_name != column.name {
             continue;
         }
 
-        match left_table {
+        match table_name {
             None => return Ok(tuple.fields[column.offset]),
-            Some(ref table_name) => {
-                if table_name == &column.table_name {
+            Some(ref tbl_name) => {
+                if tbl_name == &column.table_name {
                     return Ok(tuple.fields[column.offset]);
                 }
             },
